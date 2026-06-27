95f4eb5945a01a3315e4b09bbed6788b0fcd42d3
Axercs <alexiscv81@gmail.com>
2026-06-26 22:09:10 -0600

    Migración de MYSQL a MariaDB1

--- src/main/java/mx/edu/diseniosistemas/restaurante/util/ConexionBD.java
+++ src/main/java/mx/edu/diseniosistemas/restaurante/util/ConexionBD.java
@@ -1,22 +1,23 @@
 package mx.edu.diseniosistemas.restaurante.util;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
 public final class ConexionBD {
-    private static final String URL = "jdbc:mysql://localhost:3306/restaurante_pos_db?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
-    private static final String USER = "restaurante_user";
-    private static final String PASSWORD = "restaurante_pass";
+    private static final String JNDI_NAME = "jdbc/restauranteDB";
 
     private ConexionBD() {}
 
     public static Connection getConnection() throws SQLException {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            throw new SQLException("No se encontró el driver de MySQL", e);
+            InitialContext ctx = new InitialContext();
+            DataSource ds = (DataSource) ctx.lookup(JNDI_NAME);
+            return ds.getConnection();
+        } catch (NamingException e) {
+            throw new SQLException("No se encontro el recurso JDBC " + JNDI_NAME, e);
         }
-        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
